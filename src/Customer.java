1b2d0ba03ecb92d9b1011ee8958c96f2b58fcdc5
Richard <rtruon1@gmail.com>
2019-04-17 17:59:26 -0400

    Version 3

--- src/Customer.java
+++ src/Customer.java
@@ -4,73 +4,64 @@ Purpose Details: Sales Lead Java Class
 Course: IST 242
 Team:  4
 Date Developed: 02/17/2019
-Last Date Changed: 02/17/2019
+Last Date Changed: 04/17/2019
 Rev:
 */
+
+import java.util.ArrayList;
 
 public class Customer {
     //---- Data Members ----//
 
     //Name
+    private int customerId;
     private String First;
     private String Last;
-
-    //Payment
-    private float CreditCard;
-    private float TradeIn;
-    private float Cash;
-
-    //Budget
-    private float Max;
-    private float Min;
+    private String phone;
 
     //Car
-    private String New;
-    private String Used;
+    private String preference;
+
 
     //---- Method Members ----//
 
     //--Name--//
+
+    public Customer(int _customerId) {
+        this. customerId = _customerId;  //Increments the ID count
+    }
+
+    //Setter and Getter for CustomerID
+    public int getCustomerId() { return customerId; }
+    public void setCustomerId(int _customerId) {this.customerId = _customerId;}
+
     //Setter and Getter for First
-    public String getFirst() { return First; }
-    public void setFirst(String _First) {this.First = _First;}
+    public String getFirst() {
+        return First;
+    }
+
+    public void setFirst(String _First) {
+        this.First = _First;
+    }
+
     //Setter and Getter for Last
+
     public String getLast() { return Last; }
     public void setLast(String _Last) {this.Last = _Last;}
 
-    //--Payment--//
-    //Setter and Getter for CreditCard
-    public float getCreditCard() { return CreditCard; }
-    public void setCreditCard(float _CreditCard) {this.CreditCard = _CreditCard;}
-    //Setter and Getter for TradeIn
-    public float getTradeIn() { return TradeIn; }
-    public void setTradeIn(float _TradeIn) {this.TradeIn = _TradeIn;}
-    //Setter and Getter for Cash
-    public float getCash() { return Cash; }
-    public void setCash(float _Cash) {this.Cash = _Cash;}
+    public String getphone() { return phone; }
+    public void setphone(String _phone) {this.phone = _phone;}
 
-    //--Budget--//
-    //Setter and Getter for Max
-    public float getMax() { return Max; }
-    public void setMax(float _Max) {this.Max = _Max;}
-    //Setter and Getter for Min
-    public float getMin() { return Min; }
-    public void setMin(float _Min) {this.Min = _Min;}
+    public String getpreference() { return preference; }
+    public void setpreference(String _preference) {this.preference = _preference;}
 
-    //--Car--//
-    //Setter and Getter for New
-    public String getNew() { return New; }
-    public void setNew(String _New) {this.New = _New;}
-    //Setter and Getter for Used
-    public String getUsed() { return Used; }
-    public void setUsed(String _Used) {this.Used = _Used;}
 
-    //Arguments
-    public static void main(int[] args) {
-        //--Cars--//
-        Inventory s1 = new Inventory();
-        s1.setSedan(1);
-        System.out.println(s1.getSedan());
-
+    public static void printCustomer(ArrayList<Customer> cList) {
+        for (Customer cust : cList) {
+            System.out.println("Customer Id: " + cust.getCustomerId());
+            System.out.println("Customer Name: " + cust.getFirst() + " " + cust.getLast());
+            System.out.println("Customer Phone: " + cust.getphone());
+            System.out.println("Buying Preference: " + cust.getpreference());
+        }
     }
 }
--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,4 @@
+package com.company;
 /*
 Project: Team Project Design: Car Dealership Create the Java Classes
 Purpose Details: Sales Lead Java Class
@@ -50,109 +51,149 @@ public class Inventory {
 
     //--Cars--//
     //Setter and Getter for Sedan
-    public int getSedan() { return Sedan; }
-    public void setSedan(int _Sedan) {this.Sedan = _Sedan;}
+    public int getSedan() {
+        return Sedan; }
+    public void setSedan(int _Sedan) {
+        this.Sedan = _Sedan; }
+
     //Setter and Getter for Trucks
-    public int getTrucks() { return Trucks; }
-    public void setTrucks(int _Trucks) {this.Trucks = _Trucks;}
+    public int getTrucks() {
+        return Trucks; }
+    public void setTrucks(int _Trucks) {
+        this.Trucks = _Trucks; }
+
     //Setter and Getter for Vans
-    public int getVans() { return Vans; }
-    public void setVans(int _Vans) {this.Vans = _Vans;}
+    public int getVans() {
+        return Vans; }
+    public void setVans(int _Vans) {
+        this.Vans = _Vans; }
+
     //Setter and Getter for Hybrid
-    public int getHybrid() { return Hybrid; }
-    public void setHybrid(int _Hybrid) {this.Hybrid = _Hybrid;}
+    public int getHybrid() {
+        return Hybrid; }
+    public void setHybrid(int _Hybrid) {
+        this.Hybrid = _Hybrid; }
+
     //Setter and Getter for Sport
-    public int getSport() { return Sport; }
-    public void setSport(int _Sport) {this.Sport = _Sport;}
+    public int getSport() {
+        return Sport; }
+    public void setSport(int _Sport) {
+        this.Sport = _Sport; }
 
     //--Engines--//
     //Setter and Getter for SparkPlugs
-    public int getSparkPlugs() { return SparkPlugs; }
-    public void setSparkPlugs(int _SparkPlugs) {this.SparkPlugs = _SparkPlugs;}
+    public int getSparkPlugs() {
+        return SparkPlugs; }
+    public void setSparkPlugs(int _SparkPlugs) {
+        this.SparkPlugs = _SparkPlugs; }
+
     //Setter and Getter for Valves
-    public int getValves() { return Valves; }
-    public void setValves(int _Valves) {this.Valves = _Valves;}
+    public int getValves() {
+        return Valves; }
+    public void setValves(int _Valves) {
+        this.Valves = _Valves; }
+
     //Setter and Getter for Inquiries
-    public int getPistons() { return Pistons; }
-    public void setPistons(int _Pistons) {this.Pistons = _Pistons;}
+    public int getPistons() {
+        return Pistons; }
+    public void setPistons(int _Pistons) {
+        this.Pistons = _Pistons; }
+
     //Setter and Getter for PistonRings
-    public int getPistonRings() { return PistonRings; }
-    public void setPistonRings(int _PistonRings) {this.PistonRings = _PistonRings;}
+    public int getPistonRings() {
+        return PistonRings; }
+    public void setPistonRings(int _PistonRings) {
+        this.PistonRings = _PistonRings; }
+
     //Setter and Getter for ContactInformation
-    public int getConnectingRods() { return ConnectingRods; }
-    public void setConnectingRods(int _ConnectingRods) {this.ConnectingRods = _ConnectingRods;}
+    public int getConnectingRods() {
+        return ConnectingRods; }
+    public void setConnectingRods(int _ConnectingRods) {
+        this.ConnectingRods = _ConnectingRods; }
 
     //--Accessories--//
     //Setter and Getter for Interior
-    public int getInterior() { return Interior; }
-    public void setInterior(int _Interior) {this.Interior = _Interior;}
+    public int getInterior() {
+        return Interior; }
+    public void setInterior(int _Interior) {
+        this.Interior = _Interior; }
+
     //Setter and Getter for Exterior
-    public int getExterior() { return Exterior; }
-    public void setExterior(int _Exterior) {this.Exterior = _Exterior;}
+    public int getExterior() {
+        return Exterior; }
+    public void setExterior(int _Exterior) {
+        this.Exterior = _Exterior; }
+
     //Setter and Getter for Lighting
-    public int getLighting() { return Lighting; }
-    public void setLighting(int _Lighting) {this.Lighting = _Lighting;}
+    public int getLighting() {
+        return Lighting; }
+    public void setLighting(int _Lighting) {
+        this.Lighting = _Lighting; }
+
     //Setter and Getter for Towing
-    public int getTowing() { return Towing; }
-    public void setTowing(int _Towing) {this.Towing = _Towing;}
+    public int getTowing() {
+        return Towing; }
+    public void setTowing(int _Towing) {
+        this.Towing = _Towing; }
 
     //--Batteries--//
     //Setter and Getter for Duralast
-    public int getDuralast() { return Duralast; }
-    public void setDuralast(int _Duralast) {this.Duralast = _Duralast;}
+    public int getDuralast() {
+        return Duralast; }
+    public void setDuralast(int _Duralast) {
+        this.Duralast = _Duralast; }
+
     //Setter and Getter for Odyssey
-    public int getOdyssey() { return Odyssey; }
-    public void setOdyssey(int _Odyssey) {this.Odyssey = _Odyssey;}
+    public int getOdyssey() {
+        return Odyssey; }
+    public void setOdyssey(int _Odyssey) {
+        this.Odyssey = _Odyssey; }
+
     //Setter and Getter for Optima
-    public int getOptima() { return Optima; }
-    public void setOptima(int _Optima) {this.Optima = _Optima;}
+    public int getOptima() {
+        return Optima; }
+    public void setOptima(int _Optima) {
+        this.Optima = _Optima; }
 
     //--Tires--//
     //Setter and Getter for Goodyear
-    public int getGoodyear() { return Goodyear; }
-    public void setGoodyear(int _Goodyear) {this.Goodyear = _Goodyear;}
+    public int getGoodyear() {
+        return Goodyear; }
+    public void setGoodyear(int _Goodyear) {
+        this.Goodyear = _Goodyear; }
+
     //Setter and Getter for Firesone
-    public int getFiresone() { return Firesone; }
-    public void setFiresone(int _Firesone) {this.Firesone = _Firesone;}
+    public int getFiresone() {
+        return Firesone; }
+    public void setFiresone(int _Firesone) {
+        this.Firesone = _Firesone; }
+
     //Setter and Getter for Bridgesone
-    public int getBridgesone() { return Bridgesone; }
-    public void setBridgesone(int _Bridgesone) {this.Bridgesone = _Bridgesone;}
+    public int getBridgesone() {
+        return Bridgesone; }
+    public void setBridgesone(int _Bridgesone) {
+        this.Bridgesone = _Bridgesone; }
+
     //Setter and Getter for Michelin
-    public int getMichelin() { return Michelin; }
-    public void setMichelin(int _Michelin) {this.Michelin = _Michelin;}
+    public int getMichelin() {
+        return Michelin; }
+    public void setMichelin(int _Michelin) {
+        this.Michelin = _Michelin; }
 
     //--Brakes--//
     //Setter and Getter for Pads
-    public int getPads() { return Pads; }
-    public void setPads(int _Pads) {this.Pads = _Pads;}
+    public int getPads() {
+        return Pads; }
+    public void setPads(int _Pads) {
+        this.Pads = _Pads; }
+
     //Setter and Getter for RotorsCalipers
-    public int getRotorsCalipers() { return RotorsCalipers; }
-    public void setRotorsCalipers(int _RotorsCalipers) {this.RotorsCalipers = _RotorsCalipers;}
+    public int getRotorsCalipers() {
+        return RotorsCalipers; }
+    public void setRotorsCalipers(int _RotorsCalipers) {
+        this.RotorsCalipers = _RotorsCalipers; }
 
     //Arguments
-    public static void main(int[] args) {
-        //--Cars--//
-        Inventory s1 = new Inventory();
-        s1.setSedan(1);
-        System.out.println(s1.getSedan());
-
-        Inventory s2 = new Inventory();
-        s2.setTrucks(1);
-        System.out.println(s2.getTrucks());
-
-        Inventory s3 = new Inventory();
-        s3.setVans(1);
-        System.out.println(s3.getVans());
-
-        Inventory s4 = new Inventory();
-        s4.setHybrid(1);
-        System.out.println(s4.getHybrid());
-
-        Inventory s5 = new Inventory();
-        s5.setSport(1);
-        System.out.println(s5.getSport());
-        
 
 
-    }
 }
